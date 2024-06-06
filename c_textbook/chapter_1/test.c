7bd2ccc9064e631d0cf2d3b48fb55d6fe5cdabe2
Hong Tan <dozerwatch@gmail.com>
2024-06-06 16:57:23 -0700

    Add info

--- c_textbook/chapter_1/test.c
+++ c_textbook/chapter_1/test.c
@@ -1,54 +1,52 @@
 #include <stdio.h>
 
-#define MAXLINE 1000
+#define MAXLINE 1000   /* maximum input line length */
 
 int _getline(char [], int);
+void _remove(char [], int);
 
-void reverse(char []);
-
+/* print the longest input line */
 int main() {
 
-    int len;
-    char line[MAXLINE];
+    int len;                /* current line length */
+    char line[MAXLINE];     // current input line
 
     while ((len = _getline(line, MAXLINE)) > 0) {
-        reverse(line);
-        printf("%s", line);
+        _remove(line, len);
     }
 
-    return 0;
+    return 0; 
 }
+/* getline:  read a line into s, return length  */
+int _getline(char s[],int lim) {
 
-int _getline(char s[], int lim)
-{
     int c, i;
+
     for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
         s[i] = c;
+
     if (c == '\n') {
         s[i] = c;
         ++i; 
     }
+
     s[i] = '\0';
-    return i; 
+
+    return i;
 }
 
-void reverse(char s[]) {
-    int i, j;
-    char temp;
-
-    for (i = 0; s[i] != '\0'; ++i);
-
-    --i;
-
-    if (s[i] == '\n') --i;
-
-    j = 0;
-
-    while (j < i) {
-        temp = s[j];
-        s[j] = s[i];
-        s[i] = temp;
-        ++j;
-        --i;
-    } 
+void _remove(char line[], int i) {
+    if (i == 1) {
+        if (line[0] != '\n')
+            printf("%s", line);
+    }
+    else if (i > 1) {
+        i -= 2;
+        while (line[i] == ' ' || line[i] == '\t') {
+            line[i] = '\0';
+            --i;
+        }
+        for (int j = 0; j <= i; ++j)
+            printf("%d ", line[j]);
+    }
 }
