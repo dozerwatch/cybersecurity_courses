12b79704f7e8afc7dfea5179e1b35b20a82aa903
Hong Tan <dozerwatch@gmail.com>
2024-06-07 14:20:50 -0700

    Add info

--- c_textbook/chapter_1/test.c
+++ c_textbook/chapter_1/test.c
@@ -1,76 +1,40 @@
 #include <stdio.h>
-
 #define MAXLINE 1000   /* maximum input line length */
-
-int _getline(char [], int);
-void _remove(char [], int);
-void _reverse(char []);
-
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
 /* print the longest input line */
-int main() {
-
-    int len;                /* current line length */
-    char line[MAXLINE];     // current input line
-
-    while ((len = _getline(line, MAXLINE)) > 0) {
-        _reverse(line);
-        printf("%s", line);
+main()
+{
+int len;
+int max;
+char line[MAXLINE];
+char longest[MAXLINE]; /* longest line saved here */
+/* current line length */
+/* maximum length seen so far */
+max = 0;
+while ((len = getline(line, MAXLINE)) > 0)
+    if (len > max) {
+        max = len;
+        copy(longest, line);
     }
-
-    return 0; 
-}
-/* getline:  read a line into s, return length  */
-int _getline(char s[],int lim) {
-
-    int c, i;
-
-    for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-        s[i] = c;
-
-    if (c == '\n') {
-        s[i] = c;
-        ++i; 
-    }
-
-    s[i] = '\0';
-
-    return i;
-}
-
-void _remove(char line[], int i) {
-    if (i == 1) {
-        if (line[0] != '\n')
-            printf("%s", line);
-    }
-    else if (i > 1) {
-        i -= 2;
-        while (line[i] == ' ' || line[i] == '\t') {
-            line[i] = '\0';
-            --i;
-        }
-        for (int j = 0; j <= i; ++j)
-            printf("%d ", line[j]);
-    }
-}
-
-void _reverse(char s[]) {
-    
-    int i;
-    char temp;
-
-    for (i = 0; s[i] != '\0'; ++i);
-
-    --i;
-
-    if (s[i] == '\n') --i;
-
-    int j = 0;
-
-    while (j < i) {
-        temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-        --i;
-        ++j; 
-    }
-}
+if(max>0) /*therewasaline*/ printf("%s", longest);
+/* current input line */
+return 0; }
+   /* getline:  read a line into s, return length  */
+   int getline(char s[],int lim)
+{
+int c, i;
+       for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+           s[i] = c;
+       if (c == '\n') {
+           s[i] = c;
+++i; }
+s[i] = '\0';
+return i; }
+   /* copy:  copy 'from' into 'to'; assume to is big enough */
+   void copy(char to[], char from[])
+{
+int i;
+       i = 0;
+       while ((to[i] = from[i]) != '\0')
+++i;
