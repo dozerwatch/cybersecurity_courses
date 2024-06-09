a0926497df716f1810334b8f6b8e2f3812cf6414
Hong Tan <dozerwatch@gmail.com>
2024-06-09 16:45:46 -0700

    Add info

--- c_textbook/chapter_1/test.c
+++ c_textbook/chapter_1/test.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 
-#define WSPT 8      // White space per tab
+#define TAB 8   // Tab stop every 8th column
+#define IN 1    // In a string of blanks
+#define OUT 0   // Outside a string of blanks
 
-/* print the longest input line */
 int main() {
 
-    int c;
-    int count = 0;
+    int c, state, tabs, spaces;
+    int space_count = 0;
 
+    state = OUT;
     while ((c = getchar()) != EOF) {
-        if (c == '\t') {
-            for (int i = 0; i < WSPT - (count % WSPT); ++i)
-                putchar(' ');
-            count = 0;
+        if (c == ' ' && state == IN)
+            space_count++;
+        else if (c == ' ') {
+            state = IN;
+            space_count++;
         }
         else {
+            state = OUT;
+            if (space_count > 0) {
+                tabs = space_count / TAB;
+                spaces = space_count % TAB;
+                space_count = 0;
+                for (int i = 0; i < tabs; ++i) putchar('\t');
+                for (int i = 0; i < spaces; ++i) putchar(' ');
+            }
             putchar(c);
-            if (c == '\n') count = 0;
-            else count++;
         }
     }
 
-    return 0; 
+    return 0;
 }
+
+// I FORGOT ABOUT TAB STOPS! NEED TO FACTOR THEM IN FOR THE COUNT OF TABS!
